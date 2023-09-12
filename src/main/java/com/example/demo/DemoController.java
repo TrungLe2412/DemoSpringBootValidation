2c166461008f0331f1b65ba4ac327338786ecef4
Le Trung <trungle2001yf@gmail.com>
2023-09-12 21:17:14 +0700

    123

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -16,27 +16,16 @@ import java.util.Map;
 
 public class DemoController {
 
-    @PostMapping("/11")
+    @PostMapping("/addUser")
     public String createUser(@RequestBody @Valid DemoDto demoDto, BindingResult bindingResult) {
-        // Khi đã vào được trong đây thì userDto luôn hợp lệ
-        // BindingResult bindingResult là một tham số quan trọng trong Spring Framework để xử lý lỗi liên quan đến dữ liệu người dùng nhập vào
-        //va  xu ly theo cach ban muon
-        if (bindingResult.hasErrors()) {
-            // Xử lý lỗi ở đây, ví dụ gửi trả lỗi tới người dùng hoặc thực hiện các hành động cần thiết
-            Map<String, String> errors= new HashMap<>();
-
-            bindingResult.getFieldErrors().forEach(
-                    error -> errors.put(error.getField(), error.getDefaultMessage())
-            );
-
-            String errorMsg= "";
-
-            for(String key: errors.keySet()){
-                errorMsg+= "Lỗi ở: " + key + ", lí do: " + errors.get(key) + "\n";
-            }
+        String errorMsg = ValidationErrorHandler.processBindingResult(bindingResult);
+        if (errorMsg != null) {
             return errorMsg;
         }
+        //gọi xuống service layer
+        System.out.println(demoDto);
         return null ;
     }
+
 
 }
