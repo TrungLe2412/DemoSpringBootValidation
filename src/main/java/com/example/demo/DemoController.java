e4512a259a6e70c697946bb0913a423ed46e3ab5
Le Trung <trungle2001yf@gmail.com>
2023-09-12 21:33:49 +0700

    123

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -17,12 +17,7 @@ import java.util.Map;
 public class DemoController {
 
     @PostMapping("/addUser")
-    public String createUser(@RequestBody @Valid DemoDto demoDto, BindingResult bindingResult) {
-        String errorMsg = ValidationErrorHandler.processBindingResult(bindingResult);
-        if (errorMsg != null) {
-            return errorMsg;
-        }
-        //gọi xuống service layer
+    public String createUser(@RequestBody @Valid DemoDto demoDto) {
         System.out.println(demoDto);
         return null ;
     }
